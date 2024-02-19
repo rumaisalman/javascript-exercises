33b2684e550b8a67d96fb9c7194925a8f5c5fccd
rumaisalman <rumi.salman813@gmail.com>
2024-02-19 19:59:50 +0500

    update 10.ts

---
+++ 10.ts
@@ -0,0 +1,8 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+const name = " ali \n \t";
+//This is a trim funtion.
+console.log(name.trim());
+//This code prints my fav number.
+const favnum = 3;
+const message = `My favourite number is ${favnum}`;
+console.log(message);
