4749d67460661e4f269d28c568c6f1dc6d21d6d0
rumaisalman <rumi.salman813@gmail.com>
2024-02-27 00:50:34 +0500

    update 45.ts

--- 45.ts
+++ 45.ts
@@ -1,14 +1,17 @@
-// Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
-let car={
-    manufacturer:"",model_name:""
-}
+let car = {
+  manufacturer: "",
+  model_name: "",
+};
 
-function car_info_add(manufacturer:,model_name:,additional:){
-car.manufacturer = manufacturer
-car.model_name = model_name
-car={
-    ...manufacturer,...additional
+function car_info_add(
+  manufacturer: string,
+  model_name: string,
+  color: string,
+  warranty: string
+) {
+  car.manufacturer = manufacturer;
+  car.model_name = model_name;
+  const car1 = { manufacturer, model_name, color, warranty };
+  return console.log(car1);
 }
-return console.log(car)
-}
-car_info_add("honda",'70CC',{color:'red',warranty:'2 yrs'})
+car_info_add("honda", "70CC", "red", "2 yrs");
